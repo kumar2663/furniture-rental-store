5e018a0024ad4a4300a893513d8ffda9576e8823
kumar2663 <kapalavayikumar@gmail.com>
2021-06-15 23:42:12 +0530

    upgraded config file

--- app.py
+++ app.py
@@ -271,4 +271,4 @@ def cancel():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
--- dataconfig.py
+++ dataconfig.py
@@ -7,12 +7,12 @@ class Config(object):
     MYSQL_USER = os.environ['MYSQL_USER']
     MYSQL_PASSWORD = os.environ['MYSQL_PASSWORD']
     MYSQL_DB = os.environ['MYSQL_DB']
-    MYSQL_PORT = os.environ['MYSQL_PORT']
+    MYSQL_PORT = 3306
     DEFAULT_MAIL_SENDER = os.environ['DEFAULT_MAIL_SENDER']
     SECURITY_PASSWORD_SALT = os.environ['SECURITY_PASSWORD_SALT']
     SECRET_KEY = os.environ['SECRET_KEY']
     MAIL_SERVER = os.environ['MAIL_SERVER']
-    MAIL_PORT = os.environ['MAIL_PORT']
+    MAIL_PORT = 465
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ['MAIL_USERNAME']
