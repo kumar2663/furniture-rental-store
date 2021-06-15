fc9d608dc7515dd13797407c06f5771615bdf659
kumar2663 <kapalavayikumar@gmail.com>
2021-06-15 23:32:34 +0530

    Added configuration file

--- dataconfig.py
+++ dataconfig.py
@@ -1,21 +1,20 @@
 import os
 
-
 class Config(object):
     DEBUG = False
     TESTING = False
-    MYSQL_HOST = os.environ["MYSQL_HOST"]
-    MYSQL_USER = os.environ["MYSQL_USER"]
-    MYSQL_PASSWORD = os.environ["MYSQL_PASSWORD"]
-    MYSQL_DB = os.environ["MYSQL_DB"]
-    MYSQL_PORT = os.environ["MYSQL_PORT"]
-    DEFAULT_MAIL_SENDER = os.environ["DEFAULT_MAIL_SENDER"]
-    SECURITY_PASSWORD_SALT = os.environ["SECURITY_PASSWORD_SALT"]
-    SECRET_KEY = os.environ["SECRET_KEY"]
-    MAIL_SERVER = os.environ["MAIL_SERVER"]
-    MAIL_PORT = os.environ["MAIL_PORT"]
+    MYSQL_HOST = os.environ['MYSQL_HOST']
+    MYSQL_USER = os.environ['MYSQL_USER']
+    MYSQL_PASSWORD = os.environ['MYSQL_PASSWORD']
+    MYSQL_DB = os.environ['MYSQL_DB']
+    MYSQL_PORT = os.environ['MYSQL_PORT']
+    DEFAULT_MAIL_SENDER = os.environ['DEFAULT_MAIL_SENDER']
+    SECURITY_PASSWORD_SALT = os.environ['SECURITY_PASSWORD_SALT']
+    SECRET_KEY = os.environ['SECRET_KEY']
+    MAIL_SERVER = os.environ['MAIL_SERVER']
+    MAIL_PORT = os.environ['MAIL_PORT']
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
-    MAIL_USERNAME = os.environ["MAIL_USERNAME"]
-    MAIL_PASSWORD = os.environ["MAIL_PASSWORD"]
-    STRIPE_SECRET_KEY = os.environ["STRIPE_SECRET_KEY"]
+    MAIL_USERNAME = os.environ['MAIL_USERNAME']
+    MAIL_PASSWORD = os.environ['MAIL_PASSWORD']
+    STRIPE_SECRET_KEY = os.environ['STRIPE_SECRET_KEY']
